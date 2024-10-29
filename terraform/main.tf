d2e6fd9d655249fc651d212ed6fb60dc9f41f2ce
aadhithbala <aadhi66613@gmail.com>
2024-10-29 20:20:44 +0530

    Add database module to terraform and change name

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,3 +70,31 @@ resource "aws_security_group" "todo_db_access_sg" {
     Name = "todo-db-access-sg"
   }
 }
+
+# RDS MYSQL Instance
+
+resource "aws_db_instance" "todo_app_db" {
+  identifier        = "todo-app-db"
+  engine            = "mysql"
+  engine_version    = "8.0.39"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+
+  db_name  = "todo_app_db"
+  username = "admin"
+  password = "white123" #TODO: Fix this - Need to figure out how to pass this dynamically
+
+  vpc_security_group_ids = [aws_security_group.todo_db_access_sg.id]
+  
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  multi_az               = false
+  
+  backup_retention_period = 0  # Disable automated backups to stay in free tier
+
+  performance_insights_enabled = false
+  
+  tags = {
+    Name = "todo-app-db"
+  }
+}
