5cf703bb1105dc5a7e64d9c85b853f661a31a377
aadhithbala <aadhi66613@gmail.com>
2024-11-01 16:16:11 +0530

    setup ecs-cluster.tf file

---
+++ terraform/ecs-cluster.tf
@@ -0,0 +1,109 @@
+# ECS Cluster
+resource "aws_ecs_cluster" "todo_app_cluster" {
+  name = "todo-app-cluster"
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+}
+
+# Launch Template for ECS EC2 instances
+resource "aws_launch_template" "ecs_lt" {
+  name_prefix   = "ecs-template"
+  image_id      = "ami-04a37924ffe27da53"  # Amazon Linux 2 ECS-optimized AMI (update for your region)
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    security_groups            = [aws_security_group.todo_app_access_sg.id]
+  }
+
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 8 
+      volume_type = "gp2"
+      delete_on_termination = true
+    }
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo "ECS_CLUSTER=${aws_ecs_cluster.todo_app_cluster.name}" >> /etc/ecs/ecs.config
+              # Enable container instance draining to properly handle tasks during scale-in
+              echo "ECS_ENABLE_CONTAINER_METADATA=true" >> /etc/ecs/ecs.config
+              echo "ECS_ENABLE_SPOT_INSTANCE_DRAINING=true" >> /etc/ecs/ecs.config
+              EOF
+  )
+
+  tags = {
+    Name = "todo-app-ecs-lt"
+  }
+
+  # Enable detailed monitoring = false to stay within free tier
+  monitoring {
+    enabled = false
+  }
+}
+
+# Auto Scaling Group for ECS
+resource "aws_autoscaling_group" "ecs_asg" {
+  name                = "todo-app-ecs-asg"
+  desired_capacity    = 2
+  max_size           = 3
+  min_size           = 2
+  vpc_zone_identifier = [data.aws_subnet_ids.default.ids] 
+
+  launch_template {
+    id      = aws_launch_template.ecs_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "todo-app-ecs-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+}
+
+# Capacity Provider
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+  name = "todo-app-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+    
+    managed_scaling {
+      maximum_scaling_step_size = 1
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity          = 100
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.todo_app_cluster.name
+
+  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+  }
+}
+
+# Data source for VPC subnet IDs
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
