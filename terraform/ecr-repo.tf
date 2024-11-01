b3696d291e6f4ac6f4acefbc0e0f3880c62d5729
aadhithbala <aadhi66613@gmail.com>
2024-11-01 17:31:29 +0530

    Add lifecycle policy to keep only latest 3 images

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -6,3 +6,26 @@ resource "aws_ecr_repository" "todo_app" {
     scan_on_push = false
   }
 }
+
+
+#Lifecycle policy to keep the private repo size within 500MB
+resource "aws_ecr_lifecycle_policy" "todo_app_policy" {
+  repository = aws_ecr_repository.todo_app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 3 images"
+        selection = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
