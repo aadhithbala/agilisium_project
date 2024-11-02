d85fc8144d76517696ee3153471546a8a246ba3f
aadhithbala <aadhi66613@gmail.com>
2024-11-02 15:45:21 +0530

    Add another filter to subnets
    t2.micro is not available in ap-south-1c so ignoring the subnet to prevent ASG from throwing error

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,4 +9,9 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+
+    filter {
+    name   = "availability-zone"
+    values = ["ap-south-1a", "ap-south-1b"]
+  }
 }
