4e863ed5708bc37e8a886a554341db5254ad7f83
aadhithbala <aadhi66613@gmail.com>
2024-11-03 16:10:52 +0530

    Modify to include remote state for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,14 @@
 # Provider configuration
 terraform {
+
+  backend "s3" {
+    bucket = var.terraform_state_bucket
+    key    = "terraform/terraform.tfstate"    # Path within the bucket for the state file
+    region = "ap-south-1"                 # AWS region for the bucket
+    encrypt = true                        # Enable encryption for state file
+  }
+
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
