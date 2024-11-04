409c104f7379c8d112aac04c19bfaee4baf3f6ac
aadhithbala <aadhi66613@gmail.com>
2024-11-04 14:51:34 +0530

    Demo

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ require("./app/routes/turorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Todo App is running on port ${PORT}.`);
 });
